59f8f21bfc65164659998a9f2f49c30942fa0160
Qais Yousef <qyousef@google.com>
2023-02-20 19:29:40 +0000

    vh: sched: Filter out uclamp min/max at enqueue/dequeue task fair
    Almost taken as-is from aosp/1914696.
    
    We use vendor hook that was placed at the exact same location. But we
    had to invert the inc/dec logic because GKI already unconditionally
    calls uclamp_rq_inc/dec_id() at enqueue/dequeue_task() in core.c.
    
    To avoid having to add more hooks - just keep GKI as is, but reverse the
    logic to decrement if the filter says we can ignore uclamp_min/max for
    this task. And restore at dequeue by calling uclamp_rq_inc_id().
    
    There's also a potential clash with iowait boost. GKI could call
    cpufreq_update_util() due to iowait boost - this not a problem on its
    own, but the filter mechanism in schedutil/sched_pixel could potentially
    mean our subsequent call to cpufreq_update_util() will be ignored.
    
    And since GKI already has taken uclamp_min/max into account (the
    inc/dec explanation above) - we could end up requesting the wrong
    frequency and not being able to override it.
    
    We probably should use our vh hook to override iowait boost to defer the
    actual freq update to this new call site.
    
    Better to handle as a separate patch on top.
    
    Bug: 261695814
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: Ic6a2d5886b46988672ecf3ca8a6f81552d095951
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- drivers/soc/google/vh/kernel/sched/init.c
+++ drivers/soc/google/vh/kernel/sched/init.c
@@ -55,6 +55,8 @@ extern void rvh_update_rt_rq_load_avg_pixel_mod(void *data, u64 now, struct rq *
 extern void rvh_set_task_cpu_pixel_mod(void *data, struct task_struct *p, unsigned int new_cpu);
 extern void rvh_enqueue_task_pixel_mod(void *data, struct rq *rq, struct task_struct *p, int flags);
 extern void rvh_dequeue_task_pixel_mod(void *data, struct rq *rq, struct task_struct *p, int flags);
+extern void rvh_enqueue_task_fair_pixel_mod(void *data, struct rq *rq, struct task_struct *p, int flags);
+extern void rvh_dequeue_task_fair_pixel_mod(void *data, struct rq *rq, struct task_struct *p, int flags);
 
 extern void vh_binder_set_priority_pixel_mod(void *data, struct binder_transaction *t,
 	struct task_struct *task);
