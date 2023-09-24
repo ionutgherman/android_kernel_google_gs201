b277267e71aa4a268c7cadaf6b55032a47a8e9b5
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-25 07:40:20 +0800

    vh: sched: Fix `detach_important_task` heavy task check
    Change-Id: I7f3882557b09f1d4fc9a5b59bdd9d66c1d312a02
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- drivers/soc/google/vh/kernel/sched/fair.c
+++ drivers/soc/google/vh/kernel/sched/fair.c
@@ -2545,7 +2545,7 @@ static struct task_struct *detach_important_task(struct rq *src_rq, int dst_cpu)
 
 	list_for_each_entry_reverse(p, &src_rq->cfs_tasks, se.group_node) {
 		struct vendor_task_struct *vp = get_vendor_task_struct(p);
-		bool is_heavy_tasks = get_prefer_idle(p) || get_prefer_high_cap(p);
+		bool is_heavy_task = get_prefer_idle(p) && get_prefer_high_cap(p);
 		bool is_important = false;
 
 		if (!cpumask_test_cpu(dst_cpu, p->cpus_ptr))
@@ -2554,12 +2554,12 @@ static struct task_struct *detach_important_task(struct rq *src_rq, int dst_cpu)
 		if (task_running(src_rq, p))
 			continue;
 
-		if (!is_heavy_tasks)
+		if (!is_heavy_task)
 			continue;
 
 		if ((vp && vp->uclamp_fork_reset) 
 		    || uclamp_eff_value(p, UCLAMP_MIN) > 0 
-		    || is_heavy_tasks)
+		    || is_heavy_task)
 			is_important = true;
 
 		if (!is_important)
